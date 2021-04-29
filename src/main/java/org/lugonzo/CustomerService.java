dd5bfb76d1a34a689b51fdfa39c7a7ce56748140
Abuu001 <abulugz@gmail.com>
2021-04-30 00:05:59 +0300

    Add customer service class with props

--- src/main/java/org/lugonzo/CustomerService.java
+++ src/main/java/org/lugonzo/CustomerService.java
@@ -1,2 +1,31 @@
-package org.lugonzo;public class CustomerService {
+package org.lugonzo;
+
+public class CustomerService {
+
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public  void printName(){
+        System.out.println("Customer name :" + this.name);
+    }
+
+    public  void printUrl(){
+        System.out.println("URL link :" + this.url);
+    }
 }
