27068956079c23537c036820a06603b11bb974fc
Abuu001 <abulugz@gmail.com>
2021-05-01 00:02:42 +0300

    Added new class

--- src/main/java/org/lugonzo/Learner.java
+++ src/main/java/org/lugonzo/Learner.java
@@ -1,2 +1,27 @@
-package org.lugonzo;public class Learner {
+package org.lugonzo;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("learnerBean")
+public class Learner {
+
+    @Value("${learner.name}")
+    private String name;
+
+    @Value("${learner.interestedCourse}")
+    private  String interestedCourse;
+    private String hobby;
+
+    @Value("${learner.hobby}")
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public void displayLearnerInfo(){
+        System.out.println("Student name : "+name);
+        System.out.println("Student courses Enrolled :" + interestedCourse);
+        System.out.println("Student hobby : " +hobby);
+    }
 }
