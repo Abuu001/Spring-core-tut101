75c31793a6e8bad263fbabe7edf4236f5b5f9b7d
Abuu001 <abulugz@gmail.com>
2021-04-30 21:30:14 +0300

    md

--- src/main/java/org/lugonzo/MathTeacher.java
+++ src/main/java/org/lugonzo/MathTeacher.java
@@ -1,2 +1,15 @@
-package org.lugonzo;public class MathTeacher {
+package org.lugonzo;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary  //when we use @Primary it sets the class as a default when injected in other class
+public class MathTeacher implements Teacher{
+
+    @Override
+    public void teach() {
+        System.out.println(" Hi am your math teacher");
+        System.out.println("My name is Sourav");
+    }
 }
