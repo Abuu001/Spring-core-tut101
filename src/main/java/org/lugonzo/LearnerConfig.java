7a71341058c033c2cf56f383a8ef531622e41c7f
Abuu001 <abulugz@gmail.com>
2021-05-01 00:05:47 +0300

    configured learner properties path

--- src/main/java/org/lugonzo/LearnerConfig.java
+++ src/main/java/org/lugonzo/LearnerConfig.java
@@ -2,10 +2,12 @@ package org.lugonzo;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = "org.lugonzo")
-public class learnerConfig {
+@PropertySource("learnerBeanInfo.properties")
+public class LearnerConfig {
 
 
 }
