9375128cc862668351471e9048eeddb863c7ad6e
Abuu001 <abulugz@gmail.com>
2021-04-30 21:28:46 +0300

    configured employee beans annotations

--- src/main/java/org/lugonzo/EmployeeConfig.java
+++ src/main/java/org/lugonzo/EmployeeConfig.java
@@ -1,2 +1,28 @@
-package org.lugonzo;public class EmployeeConfig {
+package org.lugonzo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmployeeConfig {
+
+    @Bean
+    public  Address empAddress(){
+        Address addr = new Address();
+        addr.setCity("Nairobi");
+        addr.setState("Kenya");
+        addr.setZipCode(78238913);
+        return addr;
+    }
+
+    @Bean
+    public Employee employee101(){
+        Employee emp =new Employee();
+        emp.seteName("Brenda");
+        emp.setEid(103);
+        emp.seteSalary(89_000);
+        emp.setAddress(empAddress());
+
+        return emp;
+    }
 }
